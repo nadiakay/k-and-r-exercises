064ce2417d9527d68116f51eb950e1e212e042fc
Nadia Konieczny <nadiakrakowski@gmail.com>
2022-01-26 00:26:44 -0600

    working entab

---
+++ 1-21entab.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define TAB 8
+
+int main()
+{
+  int c, cols, blanks;
+  cols = 0, blanks = 0;
+  while ((c = getchar()) != EOF)
+    if (c == ' ')
+    {
+      ++blanks;
+    }
+    else if (c == '\t')
+    {
+      blanks += (TAB - (cols + blanks) % TAB);
+    }
+    else
+    {
+      if (blanks)
+      {
+        while (blanks >= TAB - cols % TAB) {
+          blanks -= (TAB - cols % TAB);
+          cols += (TAB - cols % TAB);
+          printf("\t");
+        }
+        while (blanks > 0) {
+          --blanks;
+          ++cols;
+          printf("~");
+        }
+      }
+      putchar(c);
+      ++cols;
+    }
+  return 0;
+}
